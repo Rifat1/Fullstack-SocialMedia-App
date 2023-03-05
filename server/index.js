66559e9c22fa41558ae99b61ff28d7c6804294c3
Abdur Rahman Abul Hossain <abdurrahmanpm97@gmail.com>
2023-03-05 10:39:53 -0330

    Created errorHandler middleware

--- server/index.js
+++ server/index.js
@@ -4,17 +4,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const errorHandler = require('./middleware/errorHandler');
 
 
 app.use(cors());
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/', function (req, res, next) {
-    
+
+//will put routes here
+
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    res.status(404);
+    return next(err);
 })
 
+app.use(errorHandler);
+
+
 app.listen(PORT, () => {
-    console.log(`Listening on port `);
+    console.log(`Listening on port ${PORT}`);
 })
