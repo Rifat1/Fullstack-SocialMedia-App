c7808403a819471e49e57709865b522fd86e73df
Abdur Rahman Abul Hossain <abdurrahmanpm97@gmail.com>
2023-03-08 19:46:46 -0330

    added password hashing and comparing functionality

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -16,12 +16,37 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    profileImageLink: {
+        type: String,
+    },
     messages: [{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'Message'
     }]
 });
 
+userSchema.pre('save', async function(next) {
+    try {
+        if(!this.isModified('password')) {
+            return next();
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function(plainPassword, next) {
+    try {
+        let isMatch = await bcrypt.compare(plainPassword, this.password);
+        return isMatch;
+        
+    } catch (error) {
+        return next(error);
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 
